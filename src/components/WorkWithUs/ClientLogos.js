4bb471ae8b10e61ddf818d488d861e114d1143a7
April Zhang <april.zhang@berkeley.edu>
2023-01-17 22:10:14 -0800

    update company logos

--- src/components/WorkWithUs/ClientLogos.js
+++ src/components/WorkWithUs/ClientLogos.js
@@ -12,6 +12,12 @@ import clearskye from "../../images/work_with_us/company_logos/clearskyLogo.png"
 import karbon from "../../images/work_with_us/company_logos/karbon.png"
 import seti from "../../images/work_with_us/company_logos/seti.jpg"
 import vivensity from "../../images/work_with_us/company_logos/vivensity-logo.png"
+import intuitive_surgical from "../../images/work_with_us/company_logos/Intuitive.png"
+
+
+import american_eagle from "../../images/work_with_us/company_logos/american_eagle.png"
+import hologic from "../../images/work_with_us/company_logos/hologic.png"
+import ulta_beauty from "../../images/work_with_us/company_logos/ulta_beauty.png"
 
 export const currentClients = [
     {logo: tripadvisor1, link: "https://www.tripadvisor.com/"},
@@ -20,15 +26,19 @@ export const currentClients = [
 ];
 
 export const pastClients = [
+    {logo: ulta_beauty, link: "https://www.ulta.com/"},
+    {logo: hologic, link: "https://www.hologic.com/"},
+    {logo: american_eagle, link: "https://www.ae.com/us/en"},
+    {logo: vivensity, link: "https://www.vivensity.com/"},
+    {logo: intuitive_surgical, link: "https://www.intuitive.com/en-us"},
     {logo: tripadvisor1, link: "https://www.tripadvisor.com/"},
+    {logo: karbon, link: "https://karbonhq.com/"},
+    {logo: seti, link: "https://www.seti.org/"},
+    {logo: clearskye, link: "https://clearskye.com/"},
     {logo: mcbride, link: "https://www.mcbridesisters.com/"},
     {logo: lovo, link: "https://www.lovo.ai/"},
     {logo: paladin, link: "https://paladindrones.io/"},
     {logo: perkville, link: "https://www.perkville.com/"},
     {logo: bedrock, link: "https://www.bedrockanalytics.com/"},
-    {logo: calfit, link: "https://www.californiafamilyfitness.com/"},
-    {logo: clearskye, link: "https://clearskye.com/"},
-    {logo: karbon, link: "https://karbonhq.com/"},
-    {logo: seti, link: "https://www.seti.org/"},
-    {logo: vivensity, link: "https://www.vivensity.com/"}
+    {logo: calfit, link: "https://www.californiafamilyfitness.com/"}
 ];
